9401c5d7a2ca9f6d22a991299ada6bb43bd48c63
skylerfavors <sfavors@geo-jobe.com>
2023-04-27 09:23:57 -0500

    finished example

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # MCA Presentation Notes
 
+# Links!
+[Overview](https://developers.arcgis.com/javascript/latest/)
+[ESM ~ @arcgis/core setup](https://developers.arcgis.com/javascript/latest/es-modules/)
+
+# Prep
+things we need:
+- node / npm
+- ArcGIS account
+
--- example/src/main.ts
+++ example/src/main.ts
@@ -1,1 +1,17 @@
-import './style.css'
+import "./style.css";
+import Map from "@arcgis/core/Map.js";
+import MapView from "@arcgis/core/views/MapView.js";
+import esriConfig from "@arcgis/core/config";
+
+esriConfig.apiKey = import.meta.env.VITE_API_KEY;
+
+const map = new Map({
+  basemap: "arcgis-topographic",
+});
+
+const view = new MapView({
+  map: map,
+  center: [-118.805, 34.027],
+  zoom: 13,
+  container: "viewDiv",
+});
